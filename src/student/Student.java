a2e45ebedb00520b5bdd240c41c21a3cb933a78f
calmero <mdl.jals107@gmail.com>
2024-09-26 11:26:08 +0700

    student handle by array

--- src/student/Student.java
+++ src/student/Student.java
@@ -1,47 +1,98 @@
 package student;
 
+/**
+ * Đối tượng sinh viên.
+ */
 public class Student {
 	private String name;
 	private String id;
 	private String group;
 	private String email;
 
+	/**
+	 * Lấy tên của sinh viên.
+	 *
+	 * @return tên sinh viên.
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Đặt tên cho sinh viên.
+	 *
+	 * @param name tên sinh viên.
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Lấy ID của sinh viên.
+	 *
+	 * @return ID của sinh viên.
+	 */
 	public String getId() {
 		return id;
 	}
 
+	/**
+	 * Đặt ID cho sinh viên.
+	 *
+	 * @param id ID sinh viên.
+	 */
 	public void setId(String id) {
 		this.id = id;
 	}
 
+	/**
+	 * Lấy nhóm của sinh viên.
+	 *
+	 * @return nhóm của sinh viên.
+	 */
 	public String getGroup() {
 		return group;
 	}
 
+	/**
+	 * Đặt nhóm cho sinh viên.
+	 *
+	 * @param group nhóm của sinh viên.
+	 */
 	public void setGroup(String group) {
 		this.group = group;
 	}
 
+	/**
+	 * Lấy email của sinh viên.
+	 *
+	 * @return email của sinh viên.
+	 */
 	public String getEmail() {
 		return email;
 	}
 
+	/**
+	 * Đặt email cho sinh viên.
+	 *
+	 * @param email email sinh viên.
+	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
+	/**
+	 * lấy thông tin của sinh viên.
+	 *
+	 * @return chuỗi chứa thông tin của sinh viên.
+	 */
 	public String getInfo() {
 		return name + " - " + id + " - " + group + " - " + email;
 	}
 
+	/**
+	 * Tạo sinh viên với thông tin mặc định.
+	 */
 	public Student() {
 		this.name = "Student";
 		this.id = "000";
@@ -49,6 +100,13 @@ public class Student {
 		this.email = "uet@vnu.edu.vn";
 	}
 
+	/**
+	 * Tạo sinh viên với tên, ID và email chỉ định.
+	 *
+	 * @param name tên sinh viên.
+	 * @param id ID sinh viên.
+	 * @param email email sinh viên.
+	 */
 	public Student(String name, String id, String email) {
 		this.name = name;
 		this.id = id;
@@ -56,6 +114,11 @@ public class Student {
 		this.email = email;
 	}
 
+	/**
+	 * Tạo đối tượng sinh viên mới từ đối tượng sinh viên đã tồn tại.
+	 *
+	 * @param student đối tượng Student cần sao chép.
+	 */
 	public Student(Student student) {
 		this.name = student.getName();
 		this.id = student.getId();
@@ -63,3 +126,4 @@ public class Student {
 		this.email = student.getEmail();
 	}
 }
+
--- src/student/StudentManagement.java
+++ src/student/StudentManagement.java
@@ -1,58 +1,128 @@
 package student;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
+/**
+ * Quản lý sinh viên.
+ */
 public class StudentManagement {
 	private Student[] students;
 
+	/**
+	 * Lấy danh sách sinh viên.
+	 *
+	 * @return một mảng chứa các đối tượng Student.
+	 */
 	public Student[] getStudents() {
 		return students;
 	}
 
+	/**
+	 * Cài đặt danh sách sinh viên.
+	 *
+	 * @param students một ArrayList chứa các đối tượng Student để thiết lập.
+	 */
 	public void setStudents(Student[] students) {
 		this.students = students;
 	}
 
+	/**
+	 * Thêm một sinh viên mới.
+	 *
+	 * @param newStudent đối tượng Student cần thêm.
+	 */
 	public void addStudent(Student newStudent) {
+		if (students == null) {
+			students = new Student[0];
+		}
 		Student[] newStudents = new Student[students.length + 1];
 		System.arraycopy(students, 0, newStudents, 0, students.length);
 		newStudents[students.length] = newStudent;
 		setStudents(newStudents);
 	}
 
+	/**
+	 * Kiểm tra xem hai sinh viên có thuộc cùng một nhóm hay không.
+	 *
+	 * @param s1 sinh viên 1 .
+	 * @param s2 sinh viên 2.
+	 * @return true nếu cả hai sinh viên thuộc cùng một nhóm, ngược lại false.
+	 */
 	public static boolean sameGroup(Student s1, Student s2) {
 		return s1.getGroup().equals(s2.getGroup());
 	}
 
+	/**
+	 * Thông tin sinh viên theo nhóm.
+	 *
+	 * @return thông tin sinh viên theo nhóm tương ứng.
+	 */
 	public String studentsByGroup() {
-		String studentsByGroup = "";
-		for (int i = 0; i < students.length; i++) {
-			studentsByGroup += students[i].getInfo() + "\n";
+		StringBuilder studentsInfoByGroup = new StringBuilder();
+		Map<String, StringBuilder> studentsByGroup = new LinkedHashMap<>();
+		for (Student student : students) {
+			if (!studentsByGroup.containsKey(student.getGroup())) {
+				studentsByGroup.put(student.getGroup(), new StringBuilder());
+			}
+			studentsByGroup.get(student.getGroup()).append(student.getInfo()).append("\n");
 		}
-		return studentsByGroup;
+
+		for (Map.Entry<String, StringBuilder> entry : studentsByGroup.entrySet()) {
+			studentsInfoByGroup.append(entry.getKey() + "\n");
+			studentsInfoByGroup.append(entry.getValue());
+
+		}
+		return studentsInfoByGroup.toString();
 	}
 
+	/**
+	 * Xóa sinh viên theo ID.
+	 *
+	 * @param id ID của sinh viên cần xóa.
+	 */
 	public void removeStudent(String id) {
+		if (students == null) {
+			return;
+		}
+
 		int indexOfStudent = -1;
 		for (int i = 0; i < students.length; i++) {
-			if (students[i].getId().equals(id)) {
-//				indexOfStudent =
+			System.out.println(students.length);
+			if (id.equals(students[i].getId())) {
+				indexOfStudent = i;
+				break;
 			}
 		}
-		Student[] newStudents = new Student[students.length];
+
+		if (indexOfStudent != -1) {
+			Student[] newStudents = new Student[students.length - 1];
+			System.arraycopy(students, 0, newStudents, 0, indexOfStudent);
+			System.arraycopy(students, indexOfStudent + 1, newStudents, indexOfStudent,
+					newStudents.length - indexOfStudent);
+			students = newStudents;
+		}
 	}
 
 	public static void main(String[] args) {
-		Student student = new Student();
-		Student student2 = new Student();
-		student2.setGroup("asds");
-		System.out.println(student.getInfo());
-		System.out.println(sameGroup(student, student2));
+		Student student1 = new Student("A", "1", "1@email.com");
+		Student student2 = new Student("B", "2", "1@email.com");
+		Student student3 = new Student("C", "3", "1@email.com");
+		Student student4 = new Student("D", "4", "1@email.com");
 
-		Student[] students = { student };
-//		students[0] = null;
-		System.out.println(students);
-		System.out.println(students.length);
+		student1.setGroup("K62CC");
+		student2.setGroup("K62CC");
+
+		StudentManagement studentManagement = new StudentManagement();
+		studentManagement.addStudent(student1);
+		studentManagement.addStudent(student2);
+		studentManagement.addStudent(student3);
+		studentManagement.addStudent(student4);
+
+		studentManagement.removeStudent("1");
+
+		System.out.println(studentManagement.studentsByGroup());
+
 	}
 }
+
